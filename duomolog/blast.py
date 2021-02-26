3936e51b8d83ef10b0119f722bb4bb9f88984e46
Jeff Cole <coleti16@students.ecu.edu>
2021-02-26 18:01:49 -0500

    blast function (closes #2)

--- duomolog/blast.py
+++ duomolog/blast.py
@@ -1,3 +1,31 @@
+from io import StringIO
+from Bio.Blast.Applications import NcbimakeblastdbCommandline
+from Bio.Blast.Applications import NcbiblastpCommandline
+import pandas as pd
 
-def hellNah():
-	print("hell nah")
+def run_blast(dbFile, queryFile):
+	makeblastdbCMD = NcbimakeblastdbCommandline(dbtype="prot",
+    	input_file=dbFile)
+	makeblastdbOUT, makeblastdbERR = makeblastdbCMD()
+	blastpCMD = NcbiblastpCommandline(query=queryFile, 
+		db=dbFile, evalue=0.001, max_target_seqs=1, outfmt=6)
+	blastpOUT, blastpERR = blastpCMD()
+	blasdpDF = pd.read_csv(StringIO(blastpOUT),sep="\t"
+			names = ["qseqid","sseqid","pident","length","mismatch","gapopen","qstart","qend","sstart","send","evalue","bitscore"])
+	
+	return blasdpDF
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
