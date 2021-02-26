2d9ed8ac153e8145d3b0516540a113e492dd40c9
Jeff Cole <coleti16@students.ecu.edu>
2021-02-26 18:03:21 -0500

    mafft function (closes #4)

---
+++ duomolog/msa.py
@@ -0,0 +1,11 @@
+from Bio.Align.Applications import MafftCommandline
+from io import StringIO
+from Bio import AlignIO
+
+def run_mafft(inFile):
+	mafft_cline = MafftCommandline(input = inFile)
+	stdout, stderr = mafft_cline()
+	alignment = AlignIO.read(StringIO(stdout), "fasta")
+	# print(align)
+	return alignment
+	
