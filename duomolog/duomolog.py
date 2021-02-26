cf0b123e50adaa985bb0024915e035124dfd5857
Jeff Cole <coleti16@students.ecu.edu>
2021-02-26 07:49:33 -0500

    check if input seqs are in query (closes #1)

--- duomolog/duomolog.py
+++ duomolog/duomolog.py
@@ -73,8 +73,43 @@ class ParseCommands(object):
 		return(self.args)
 		
 
+def notRedundant(sequences,aligned=True):
+	seqs = {}
+	for record in sequences:
+		if record.seq not in seqs:
+			if aligned:
+				seq = str(record.seq).replace("-","")
+			seqs[seq] = record.id
+		else:
+			seqs = {}
+			break
+	return seqs
 
-    
+def pairRedundant(known,unknown, aligned=True):
+	nr_known = notRedundant(known)
+	nr_unknown = notRedundant(unknown)
+	
+	if nr_known == False:
+		sys.exit("input alignment contains redundant sequences, please fix and try again")
+	if nr_unknown == False:
+		sys.exit("input query contains redundant sequences, please fix and try again")
+	
+	known_seqs = set(nr_known.keys())
+	unknown_seqs = set(nr_unknown.keys())
+	shared_seqs = known_seqs & unknown_seqs
+	shared_seqs_unknown_ids = [nr_unknown[i] for i in shared_seqs]
+	return shared_seqs_unknown_ids
+	
+	
+	
+	
+
+	# print("checking if ", known, "has ")
+	# if aligned:
+		
+
+
+
 
 
 
@@ -88,10 +123,17 @@ def main():
 
 	query_file = duomolog_args.query.name
 	query = SeqIO.parse(query_file, "fasta")
-	
+
 	if subcommand == "blast_v_hmmer":
 		alignment_file = duomolog_args.aln.name
 		alignment = AlignIO.read(open(alignment_file), alignment_format)
+		sharedIDs = pairRedundant(alignment,query)
+		if sharedIDs:
+			print("Error: the following sequences were found in the query that are already in the input alignment, please remove and try again")
+			for h in sharedIDs:
+				print(h)
+			sys.exit()
+		
 		
 		
 
