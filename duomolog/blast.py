c7d193dde83eba296f26d4ade5f8fc39f88c3341
Jeff Cole <coleti16@students.ecu.edu>
2021-02-28 06:51:11 -0500

    print blast commands

--- duomolog/blast.py
+++ duomolog/blast.py
@@ -6,9 +6,12 @@ import pandas as pd
 def run_blast(dbFile, queryFile):
 	makeblastdbCMD = NcbimakeblastdbCommandline(dbtype="prot",
     	input_file=dbFile)
-	makeblastdbOUT, makeblastdbERR = makeblastdbCMD()
 	blastpCMD = NcbiblastpCommandline(query=queryFile, 
 		db=dbFile, evalue=0.001, max_target_seqs=1, outfmt=6)
+	print(makeblastdbCMD)
+	print(blastpCMD)
+	makeblastdbOUT, makeblastdbERR = makeblastdbCMD()
+	
 	blastpOUT, blastpERR = blastpCMD()
 	blasdpDF = pd.read_csv(StringIO(blastpOUT),sep="\t", names = ["qseqid","sseqid","pident","length","mismatch","gapopen","qstart","qend","sstart","send","evalue","bitscore"])
 	
